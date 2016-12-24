a93738c476a0a3d083e84cba2db8ced4281d7af6
lambo4bkfast <edgararakelyan123@gmail.com>
2016-12-23 21:04:52 -0800

    problem 12 is almost done, slight bug remains

---
+++ solutions/12.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int count_divisors(int);
+int triangular_sum(int);
+
+const int limit = 500;
+
+int main(){
+	
+	//counter for the while loop
+	int counter = 1; 
+	//hold the value of the current triangular_sum
+	int triangular_sum_num;
+	//number of factors
+	int factors;
+	
+	//when true the loop will end
+	bool sentinel = false;
+	
+	
+	while (!(sentinel)){
+		triangular_sum_num = triangular_sum(counter);
+		factors = count_divisors(triangular_sum_num);
+		
+		if (factors > 500){
+			cout << "The first triangular number to have over 500 divisors is " << triangular_sum_num;
+			sentinel = true;
+		}
+		counter++;
+	}
+	
+	
+	return 0;
+}
+
+//count the number of divisors of a number.
+int count_divisors(int k){
+	
+	int divisors;
+	
+	if (k == 1){
+		return 1;
+	}
+	else {
+		for (int i = 2; i <= k/2; i++ ){
+			if (k % i == 0){
+				divisors++;
+		}
+	}
+		//we count 1 and k as a divisor for every k, and the loop skips 1 and never reaches k.
+		divisors += 2;
+		return divisors;
+	}
+	
+}
+
+//this will count the triangular sums for the nth triangular sum, for example the 2nd triangular sum is
+//1 + 2 = 3
+int triangular_sum(int n){
+	
+	int k = 1;
+	int triangular_sum = 0;
+	
+	while (k <= n){
+		triangular_sum += k;
+		k++;
+	}
+	
+	return triangular_sum;
+}
